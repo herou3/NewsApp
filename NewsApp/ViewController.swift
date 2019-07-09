9b86edafd2a95cf77d71d234409cadddb99fe3a8
Pavel Kurilov <pkurilov@spaceotechnologies.ru>
2019-07-09 23:55:55 +0700

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Pavel Kurilov on 09/07/2019.
+//  Copyright © 2019 Pavel Kurilov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
